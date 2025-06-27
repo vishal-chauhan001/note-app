dd82ad0da8abeb6828378ce1a91a0fe37d198074
vishal-chauhan001 <vishal.c@chalo.com>
2025-06-27 16:39:14 +0530

    minor changes

--- app/src/main/java/com/example/note/presentation/components/NoteCard.kt
+++ app/src/main/java/com/example/note/presentation/components/NoteCard.kt
@@ -1,3 +1,5 @@
+package com.example.note.presentation.components
+
 import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
--- app/src/main/java/com/example/note/presentation/notes/NotesIntent.kt
+++ app/src/main/java/com/example/note/presentation/notes/NotesIntent.kt
@@ -3,6 +3,6 @@ package com.example.note.presentation.notes
 import com.example.note.domain.model.Note
 
 sealed class NotesIntent {
-    object LoadNotes : NotesIntent()
+    data object LoadNotes : NotesIntent()
     data class DeleteNote(val note: Note) : NotesIntent()
 }
